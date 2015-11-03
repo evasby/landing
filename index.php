e786ff0fbf60e9e465452566c35358b09065cb98
Vladimir Dmitriev <defurator@mail.ru>
2015-11-03 13:04:52 +0300

    header+main1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charset="UTF-8">
-  <title>Start - Главная</title>
+  <title>Landing - Главная</title>
   <link rel="stylesheet" href="css/all.css" type="text/css" />
   <link rel="stylesheet" href="dist/animate.css" type="text/css"/>
   <!--[if lt IE 9]>
@@ -17,20 +17,56 @@
   <![endif]-->
   <script src="//maps.googleapis.com/maps/api/js?v=3.exp"></script>
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  <script type="text/javascript" src="js/bg-loaded.js"></script>
-  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
-  <script type="text/javascript" src="js/jquery.bxslider.js"></script>
-  <script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
-  <script type="text/javascript" src="js/jquery.waypoints.min.js"></script>
-  <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+  <!--<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>-->
+  <script type="text/javascript" src="dist/bg-loaded.js"></script>
+  <script type="text/javascript" src="dist/jquery.placeholder.js"></script>
+  <script type="text/javascript" src="dist/jquery.bxslider.js"></script>
+  <script type="text/javascript" src="dist/jquery.colorbox-min.js"></script>
+  <script type="text/javascript" src="dist/jquery.waypoints.min.js"></script>
+  <script type="text/javascript" src="dist/jquery.scrollTo.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>  
 </head>
 <body>
-  <header class="header">
-    
+  <header id="header" class="header">
+    <div class="inner">
+      <div class="contacts">
+        <div class="contacts_phone"><p>+375 44 732 05 09</p></div>
+        <div class="contacts_address"><p>г.Минск Притыцкого 29 оф 522</p></div>
+      </div>
+      <a href="/layout/" class="webformat">Веб студия Webformat</a>
+      <nav class="menu">
+        <ul class="menu_list">
+          <li class="menu_item">
+            <a href="#header" class="menu_link">Главная</a>
+          </li>
+          <li class="menu_item">
+            <a href="#header" class="menu_link">О нас</a>
+          </li>
+          <li class="menu_item">
+            <a href="#header" class="menu_link">Портфолио</a>
+          </li>
+          <li class="menu_item">
+            <a href="#header" class="menu_link">Тарифы</a>
+          </li>
+          <li class="menu_item">
+            <a href="#header" class="menu_link">Отзывы</a>
+          </li>
+          <li class="menu_item">
+            <a href="#header" class="menu_link">Котнтакты</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
-
+  <div id="main1" class="main1">
+    <div class="inner">
+      <div class="main1_text">
+        <p><span>Создание landing page с гарантией</span></p>
+        <p><span>конверсии от 10% до 30%</span></p>
+      </div>
+      <a href="#" class="main1_link">Связаться с нами</a>
+    </div>
+  </div>
     
 
   <footer class="footer">
