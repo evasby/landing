dd683ba30d0a7986af2e6a526db90ad03654904b
Vladimir Dmitriev <defurator@mail.ru>
2015-11-04 13:12:10 +0300

    main2+main3+main4

--- index.php
+++ index.php
@@ -67,6 +67,97 @@
       <a href="#" class="main1_link">Связаться с нами</a>
     </div>
   </div>
+  <div id="main2" class="main2">
+    <div class="inner">
+      <div class="main2_title"><span>О компании Webformat</span></div>
+      <div class="main2_wrap">
+        <div class="main2_item">
+          <img src="image/down/main2-1.png" alt=""><p>Более 3 лет работы</p>
+        </div>
+        <div class="main2_item">
+          <img src="image/down/main2-2.png" alt=""><p>Конверсия от 5 до 20%</p>
+        </div>
+        <div class="main2_item">
+          <img src="image/down/main2-3.png" alt=""><p>Более 100 успешных работ</p>
+        </div>
+        <div class="main2_item">
+          <img src="image/down/main2-4.png" alt=""><p>Цена заявки от 1 у.е.</p>
+        </div>
+        <div class="main2_item">
+          <img src="image/down/main2-5.png" alt=""><p>Работаем до 100% результата</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="main3" class="main3">
+    <div class="inner">
+      <div class="main3_title"><span>Наши работы</span></div>
+      <div class="main3_wrap">
+        <a href="#" class="main3_link"><img src="image/down/main3-1.jpg" alt=""><div class="main3_text"><p>Manage your Condominium the Easy way</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-2.jpg" alt=""><div class="main3_text"><p>Search, Play and Discover</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-3.jpg" alt=""><div class="main3_text"><p>Grilled Chicken</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-4.jpg" alt=""><div class="main3_text"><p>Epic Concept and great Discovery System</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-5.jpg" alt=""><div class="main3_text"><p>Gain more traffic</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-6.jpg" alt=""><div class="main3_text"><p>Download your favorites</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-7.jpg" alt=""><div class="main3_text"><p>Global Sightseeing Made Easy</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-8.jpg" alt=""><div class="main3_text"><p>Digital Design</p></div></a>
+        <a href="#" class="main3_link"><img src="image/down/main3-9.jpg" alt=""><div class="main3_text"><p>Call of Adam</p></div></a>
+      </div>
+    </div>
+  </div>
+  <div id="main4" class="main4">
+    <div class="inner">
+      <div class="main4_title"><span>Наши тарифы</span></div>
+      <div class="main4_wrap">
+        <div class="main4_item">
+          <div class="tarif tarif__green">
+            <div class="tarif_title tarif_title__green">Начальный</div>
+            <p>Рекомендации по оптимизации</p>
+            <p>Аудит существующей рекламной компании</p>
+            <p>Определение пригодности вашей компании</p>
+            <div class="tarif_price tarif_price__green">Бесплатно</div>
+            <a href="#" class="tarif_link tarif_link_green">Заказать тариф</a>
+          </div>
+        </div>
+        <div class="main4_item">
+          <div class="tarif tarif__blue">
+            <div class="tarif_title tarif_title__blue">Оптимальный</div>
+            <p>Анализ топовых конкурентов по СНГ</p>
+            <p>Создание продающего сайта</p>
+            <p>Тех. поддержка сайта 360 дней</p>
+            <p>Настройка компаний гугл адвордс</p>
+            <p>Ведение компании 1 месяц</p>
+            <p>Ежемесячный рекламный бюджет от 300 у.е.</p>
+            <p>Индивидуальный дизайн</p>
+            <p>Установка CMS На ваш сайт</p>
+            <div class="tarif_price tarif_price__blue">899$</div>
+            <a href="#" class="tarif_link tarif_link_blue">Заказать тариф</a>
+          </div>
+        </div>
+        <div class="main4_item">
+          <div class="tarif tarif__red">
+            <div class="tarif_title tarif_title__red">Максимальный</div>
+            <p>Анализ топовых конкурентов по СНГ</p>
+            <p>Создание продающего сайта</p>
+            <p>Тех. поддержка сайта 360 дней</p>
+            <p>Настройка компаний гугл адвордс</p>
+            <p>Активная оптимизация компаний</p>
+            <p>Настройка компаний Яндекс.директ</p>
+            <p>Ведение компании 2 месяца</p>
+            <p>Ежемесячный рекламный бюджет от 500 у.е.</p>
+            <p>Дизайн премиум класса</p>
+            <p>Установка CMS На ваш сайт</p>
+            <p>Хостинг на 1 год</p>
+            <p>Домен на 1 год</p>
+            <p>А/В тестирование 1 месяц</p>
+            <p>100% окупаемость проекта</p>
+            <div class="tarif_price tarif_price__red">1 999$</div>
+            <a href="#" class="tarif_link tarif_link_red">Заказать тариф</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
     
 
   <footer class="footer">
